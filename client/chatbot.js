080d31ccfc1c3e6bb16862864aaeed49a37759f1
Felix Schwendner <felix.schwendner@werbewind.com>
2022-12-22 16:13:02 +0100

    more test sentences added, style fixed, todo list added some bugfixes

--- client/chatbot.js
+++ client/chatbot.js
@@ -2,6 +2,7 @@ var chatInputs = document.getElementsByClassName('chatbot-text');
 var content;
 var chatWindow = document.getElementById('chatbot-window');
 var chatbotInput = document.getElementById('chatbot-input');
+var chatMessageWindow = document.getElementById('chatbot-message-window');
 
 
 var messages = [];
@@ -34,41 +35,58 @@ function sendMessage(index) {
     httpReq.onreadystatechange = function () {
       if (httpReq.readyState === 4) {
         console.log(httpReq.response)
-        addMessagesToChat(JSON.parse(httpReq.response));
-        scrollToBottom(chatWindow);
+        addBotMessage(JSON.parse(httpReq.response));
+        addUserMessage(JSON.parse(httpReq.response))
+        scrollToBottom(chatMessageWindow);
       }
     }
     httpReq.send(null);
   }
 }
 
+sendMessage(1);
+
 Array.from(document.getElementsByClassName('chatbot-message')).forEach(function (msg) {
   if (msg.classList.contains('option')) {
     msg.addEventListener('click', function () {
+
       sendMessage(this.getAttribute('data-index'));
-      this.remove();
+      this.classList.remove('option');
+      this.classList.add('user-message');
     })
   }
 });
 
-function addMessagesToChat(response) {
+
+function addBotMessage(response) {
   var newBotMsg = document.createElement('div');
-  console.log(response)
   newBotMsg.innerText = response['msg'];
   newBotMsg.classList.add('chatbot-message');
   newBotMsg.classList.add('bot-message');
-  chatbotInput.appendChild(newBotMsg);
-
+  chatMessageWindow.appendChild(newBotMsg);
+}
+function addUserMessage(response) {
+  var i = 0;
   response['answers'].forEach(function(answer) {
     var newAnswer = document.createElement('div');
     newAnswer.classList.add('chatbot-message');
     newAnswer.classList.add('option');
     newAnswer.setAttribute('data-index', answer['refID']);
     newAnswer.innerText = answer['msg'];
-    newAnswer.addEventListener('click', function() {
+    newAnswer.addEventListener('click', function convertToUserMsg() {
       sendMessage(this.getAttribute('data-index'))
+      this.classList.remove('option');
+      Array.from(document.getElementsByClassName('option')).forEach(function(option) {
+       option.remove();
+      });
+      this.classList.add('user-message');
+      this.removeAttribute('style');
+      this.removeEventListener('click', convertToUserMsg);
+      this.remove();
+      chatMessageWindow.appendChild(this);
     })
     chatbotInput.appendChild(newAnswer)
+    i++;
   });
 
 
